ea5f3d944ea1026b02baaf3cd9e393e4a9067e54
HNephilim <65306305+HNephilim@users.noreply.github.com>
2020-05-27 15:26:22 -0300

    Create Codingame_ANEO.cpp
    Testes para um exercício do Codingames: https://www.codingame.com/training/medium/aneo

---
+++ Codingame_ANEO.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+
+int main()
+{
+    int speed;
+    cin >> speed; cin.ignore();
+    int lightCount;
+    cin >> lightCount; cin.ignore();
+    
+
+    for (int i = 0; i < lightCount; i++) {
+        int distance;
+        int duration;
+        cin >> distance >> duration; cin.ignore();
+        
+        bool speed_ok = false;
+        
+        while (!speed_ok){
+            
+            double time_to_travel = ((double)distance/((double)speed/3.6f));
+            for (int n = 0; time_to_travel >= n*duration; n += 2){
+                time_to_travel <= (n+1)*duration ? speed_ok = true : speed_ok = false;
+                cerr<<n<<" "<<(n+1)<<endl;
+                cerr<<distance<<" "<<duration<<" "<<speed<<endl;
+                cerr<<time_to_travel<<endl;
+            }
+            if (!speed_ok){
+                speed--;
+            }
+        }
+        
+    }
+
+    
+    cout << speed << endl;
+}
