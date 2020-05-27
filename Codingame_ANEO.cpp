3c2c7418321b91361ddcfb199a663c28159b8fd0
HNephilim <65306305+HNephilim@users.noreply.github.com>
2020-05-27 20:55:25 -0300

    Update Codingame_ANEO.cpp
    Adicionando algumas validações para comparar float

--- Codingame_ANEO.cpp
+++ Codingame_ANEO.cpp
@@ -4,46 +4,43 @@
 #include <algorithm>
 
 using namespace std;
-/* Exemplo de input
-
-50
-1
-200 15
-
-Deve retornar 50 como resposta */
-
 
 int main()
 {
     int speed;
-    cin >> speed; cin.ignore();
+    cin >> speed;
+    cin.ignore();
     int lightCount;
-    cin >> lightCount; cin.ignore();
-    
+    cin >> lightCount;
+    cin.ignore();
 
-    for (int i = 0; i < lightCount; i++) {
+    for (int i = 0; i < lightCount; i++)
+    {
         int distance;
         int duration;
-        cin >> distance >> duration; cin.ignore();
-        
+        cin >> distance >> duration;
+        cin.ignore();
+
         bool speed_ok = false;
-        
-        while (!speed_ok){
-            
-            double time_to_travel = ((double)distance/((double)speed/3.6f));
-            for (int n = 0; time_to_travel >= n*duration; n += 2){
-                time_to_travel <= (n+1)*duration ? speed_ok = true : speed_ok = false;
-                cerr<<n<<" "<<(n+1)<<endl;
-                cerr<<distance<<" "<<duration<<" "<<speed<<endl;
-                cerr<<time_to_travel<<endl;
+
+        while (!speed_ok)
+        {
+
+            double time_to_travel = ((double)distance / ((double)speed / 3.6f));
+            for (int n = 0; time_to_travel >= n * duration; n += 2)
+            {
+                float end_of_time = (n + 1) * duration;
+                time_to_travel <= end_of_time ? speed_ok = true : speed_ok = false;
+                cerr << "Round " << n * duration << " " << (n + 1) * duration << endl;
+                cerr << "Dist " << distance << " Dur " << duration << " Speed " << speed << endl;
+                cerr << "tempo pra chegar " << time_to_travel << endl;
             }
-            if (!speed_ok){
+            if (!speed_ok)
+            {
                 speed--;
             }
         }
-        
     }
 
-    
     cout << speed << endl;
 }
