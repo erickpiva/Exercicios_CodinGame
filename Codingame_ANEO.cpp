3a1ab4cb5a04884d062463faadb618ba32601f95
HNephilim <65306305+HNephilim@users.noreply.github.com>
2020-05-27 23:10:03 -0300

    Update Codingame_ANEO.cpp
    Salva as entradas todas num vetor. Depois vai checando cada entrada. Se uma falha diminui a velocidade e reinicia a análise do 0

--- Codingame_ANEO.cpp
+++ Codingame_ANEO.cpp
@@ -14,29 +14,32 @@ int main()
     cin >> lightCount;
     cin.ignore();
 
+    vector<int> distance;
+    vector<int> duration;
+
     for (int i = 0; i < lightCount; i++)
     {
-        int distance;
-        int duration;
-        cin >> distance >> duration;
-        cin.ignore();
+        int temp_distance;
+        int temp_duration;
+        cin >> temp_distance >> temp_duration;
+        distance.push_back(temp_distance);
+        duration.push_back(temp_duration);
+    }
 
-        bool speed_ok = false;
+    bool speed_ok = false; //flag to check if the actual speed works
 
-        while (!speed_ok)
+    for (int i = 0; i < lightCount; i++)
+    {
+        double time_to_travel = (distance[i] / (speed / 3.6));
+        int cycle = time_to_travel / duration[i];
+        cycle % 2 == 0 ? speed_ok = 1 : speed_ok = 0;
+
+        if (!speed_ok)
         {
-
-            double time_to_travel = (distance / (speed / 3.6));
-            int cycle = time_to_travel/duration; // Checks in which cycle the light is when arrives.
-
-            cycle % 2 == 0 ? speed_ok = 1 : speed_ok = 0; // if the integer part of the cycle is even, then it is green.
-
-            if (!speed_ok)
-            {
-                speed--;
-            }
+            speed--;
+            i = 0;
         }
     }
 
-    cout << speed << endl;
+        cout << speed << endl;
 }
