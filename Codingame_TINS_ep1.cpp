ceb052d685f8c56f0d60583b126d6bb7d51eb461
HNephilim <65306305+HNephilim@users.noreply.github.com>
2020-06-01 21:52:31 -0300

    Create Codingame_TINS_ep1.cpp

---
+++ Codingame_TINS_ep1.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+//link para exercício https://www.codingame.com/ide/puzzle/there-is-no-spoon-episode-1
+
+using namespace std;
+
+/**
+ * Don't let the machines win. You are humanity's last hope...
+ **/
+
+int main()
+{
+    int width; // the number of cells on the X axis
+    cin >> width; cin.ignore();
+    int height; // the number of cells on the Y axis
+    cin >> height; cin.ignore();
+
+    char** grid = new char*[height]; //y - axis
+    for (int i = 0; i < height; i++)
+    {
+        grid[i] = new char[width]; //x - axis
+    }
+    
+    cerr<<width<<height<<endl;
+
+    for (int i = 0; i < height; i++) 
+    {
+        string line;
+        getline(cin, line); // width characters, each either 0 or .
+        char temp[40];
+        line.copy(temp, width, 0);
+        grid[i] = temp;
+                
+    }
+
+    
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            int output[6];
+            output [0] = j;
+            output [1] = i;
+
+            //search em x axis
+            for (int k = j+1; k <= width; k++)
+            {
+
+                   
+                if (grid[i][k] == 48) // 48 is ascii for 0
+                {
+                    output[2] = k;
+                    output[3] = i;
+                    break;
+                }else 
+                {
+                output[2] = -1;
+                output[3] = -1;
+                }
+            }
+            
+            //search in y axis
+            for (int k = i+1; k <= height; k++)
+            {
+                
+                if (grid[k][j] == 48) // 48 is ascii for 0
+                {
+                    output[4] = j;
+                    output[5] = k;
+                    break;
+                }else 
+                {
+                output[4] = -1;
+                output[5] = -1;
+                }
+            }
+
+            cout<<output[0]<<" "<<output[1]<<" "<<output[2]<<" "<<output[3]<<" "<<output[4]<<" "<<output[5]<<endl;
+
+        }
+    }
+
+}
